485a024e418f1ddf68d0d0196f5a4d3484c4a397
Jhon K <kasyanov_k@list.ru>
2022-02-05 13:50:17 +0300

    there were some medium result

--- 10_type-transform/src/controller.js
+++ 10_type-transform/src/controller.js
@@ -2,6 +2,6 @@ window.controllerStudents = {
     execute: function(model, view) {
         view.setController(this);
         view.showTable(document.querySelector('.table-container'), model.students);
-        view.showNewStudentForm(document.querySelector('.form-new-student-container'));
+        view.showButtonNewStudent(document.querySelector('.form-new-student-container'));
     },
 };
--- 10_type-transform/src/view.js
+++ 10_type-transform/src/view.js
@@ -1,10 +1,10 @@
 window.viewStudents = {
-    HEADER: `<thead class="table-dark">
+    HEADER: `<thead>
         <tr>
-          <th class="fio">ФИО</th>
-          <th class="fac">Факультет</th>
-          <th class="age">ДР и возраст</th>
-          <th class="years">Годы обучения</th>
+          <th class="fio" role="button">ФИО</th>
+          <th class="fac" role="button">Факультет</th>
+          <th class="age" role="button">ДР и возраст</th>
+          <th class="years" role="button">Годы обучения</th>
         </tr>
     </thead>`,
 
@@ -43,7 +43,7 @@ window.viewStudents = {
     },
 
     renderStudentsTable: function(studentsData) {
-        const table = `<table class="table table-striped">` +
+        const table = `<table class="table table-hover">` +
             this.renderHeader() +
             this.renderRows(studentsData) +
             `</table>`;
@@ -97,7 +97,7 @@ window.viewStudents = {
 
     },
 
-    showNewStudentForm: function(container) {
+    showButtonNewStudent: function(container) {
         container.innerHTML = this.renderButtonNewStudent();
         this.setHandler(container.querySelector('.btn-new-student'), 'click', this.newStudentClick);
     },
@@ -108,5 +108,43 @@ window.viewStudents = {
 
     newStudentClick: function() {
         console.log('newStudentClick');
-    }
+        viewStudents.showFormNewStudent();
+    },
+
+    renderFormNewStudent: function() {
+        return `<form>
+									<div class="mb-3">
+										<input type="text" class="new-first-name form-control" placeholder="Имя">
+									</div>
+									<div class="mb-3">
+										<input type="text" class="new-patronymic form-control" placeholder="Отчество">
+									</div>
+									<div class="mb-3">
+										<input type="text" class="new-last-name form-control" placeholder="Фамилия">
+									</div>
+									<div class="mb-3">
+										<input type="date" class="new-born-date form-control" placeholder="дата рождения">
+									</div>
+									<div class="mb-3">
+										<input type="number" class="new-start-year form-control" min="1960" max="2100" placeholder="Год начала обучения">
+									</div>
+									<div class="mb-3">
+										<input type="text" class="new-facultet form-control" placeholder="Факультет">
+									</div>
+									<div class="mb-3">
+										<button class="btn btn-primary btn-create-new-student">Создать</button>
+									</div>
+								</form>`;
+    },
+
+    showFormNewStudent: function() {
+        let container = document.querySelector('.form-new-student-container');
+        container.innerHTML = this.renderFormNewStudent();
+        this.setHandler(container.querySelector('.btn-create-new-student'),
+            'click', this.btnCreateStudentClick);
+    },
+
+    btnCreateStudentClick: function() {
+        console.log('btnCreateStudentClick');
+    },
 };
